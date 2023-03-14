29e7871a8b6cb99316870135cf1b3563307cf0df
Vishruti Patil <vishruti2908@gmail.com>
2023-03-15 00:27:26 +0530

    Dequeue operation using LinkedList added

--- src/Data_Structure/QueueUsingLinkedList.java
+++ src/Data_Structure/QueueUsingLinkedList.java
@@ -1,28 +1,43 @@
 package Data_Structure;
 
 public class QueueUsingLinkedList {
-        Node front = null;
-        Node rear=null;
-        void enqueue(Object data) {
-            append(data);
+    Node front = null;
+    Node rear = null;
+
+    void enqueue(Object data) {
+        append(data);
+    }
+
+    void dequeue() {
+        if (front == null) {
+            System.out.println("Queue is empty");
+        } else {
+            System.out.println("\ndone");
+            front = front.next;
         }
-        void display() {
-            Node temp = front;//pointer of type Node
-            if (front == null) {
-                System.out.println("This queue is empty");}
-            while (temp != null) {
-                System.out.print(temp.data+"->");
-                temp = temp.next;
-            }
+        display();
+    }
+
+    void display() {
+        Node temp = front;//pointer of type Node
+        if (front == null) {
+            System.out.println("This queue is empty");
         }
-        void append(Object data) {
-            Node new_node = new Node(data);
-            if (front == null) {
-                front = new_node;
-                rear = new_node;
-            } else {
-                rear.next=new_node;
-                rear=new_node;
-            }
+        System.out.println("The front of queue is " + front.data);
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
         }
+    }
+
+    void append(Object data) {
+        Node new_node = new Node(data);
+        if (front == null) {
+            front = new_node;
+            rear = new_node;
+        } else {
+            rear.next = new_node;
+            rear = new_node;
+        }
+    }
 }
